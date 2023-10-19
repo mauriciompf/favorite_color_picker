0a5a061f394ba3e815d0bfb6d77d1c6c7be56952
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-19 19:57:13 -0400

    Updte randomRgba.ts

--- favorite_color_picker/src/utils/randomRgba.ts
+++ favorite_color_picker/src/utils/randomRgba.ts
@@ -1,19 +1,14 @@
 export default function randomRgba() {
-  //@ts-ignore
   const { floor, random } = Math;
-  //@ts-ignore
   const randomRGB = Array(3)
     .fill(null)
     .map(() => floor(random() * 255));
-  //@ts-ignore
   const alpha = (random() * 1).toFixed(2);
 
   const rgb = [];
   for (let i = 0; i < 3; i++) {
-    //@ts-ignore
     rgb.push(randomRGB[i]);
   }
-  //@ts-ignore
   rgb.join(", ");
 
   const rgba = `rgba(${rgb}, ${alpha})`;
