1dd568a8b9a0235df8132ab6284afe439a95f826
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-20 15:58:46 -0400

    Update saveColor.ts

--- favorite_color_picker/src/saveColor.ts
+++ favorite_color_picker/src/saveColor.ts
@@ -1,34 +1,43 @@
 import addColor from "./addColor";
 import { colorList } from "./colorList";
 import copyColor from "./utils/copyColor";
+import createElement from "./utils/createElement";
 
-const favWrap = document.querySelector(".fav-wrap") as HTMLElement;
-const fav = document.querySelector(".fav") as HTMLElement;
+const favoriteColors = document.querySelector(
+  ".favorite-colors"
+) as HTMLElement;
+const colorListElem = document.querySelector(
+  ".favorite-colors__list"
+) as HTMLElement;
 
 export default function saveColor() {
-  const getBodyBg = window
+  // Extract the current bgColor from body tag using 'style' attribute
+  const currentBodyBg = window
     .getComputedStyle(document.body)
     .getPropertyValue("background-color");
 
-  if (!colorList.includes(getBodyBg)) {
-    const item = document.createElement("li");
-    item.setAttribute("class", "item");
-    item.setAttribute("title", "Copy color");
-
-    const colorItem = document.createElement("div");
-    colorItem.setAttribute("class", "color-item");
-    colorItem.style.backgroundColor = getBodyBg;
-
-    item.textContent = getBodyBg;
-    favWrap.classList.add("active");
-
-    item.appendChild(colorItem);
-    fav.appendChild(item);
-
-    item.addEventListener("click", () => {
-      copyColor(getBodyBg);
+  // Prevent create the same color item in favorite colors
+  if (!colorList.includes(currentBodyBg)) {
+    const itemList = createElement("li", {
+      class: "favorite-colors__list-item",
+      title: "Copy color",
+    });
+    const itemColor = createElement("div", {
+      class: "favorite-colors__list-item-color",
     });
 
-    addColor(getBodyBg);
+    itemList.textContent = currentBodyBg;
+    itemColor.style.backgroundColor = currentBodyBg;
+
+    itemList.appendChild(itemColor); // li => div
+    colorListElem.appendChild(itemList); // ul => li
+
+    // Copy in clipboard
+    itemList.addEventListener("click", () => {
+      copyColor(currentBodyBg);
+    });
+
+    addColor(currentBodyBg); // Add current bgColor to colorList array
+    favoriteColors.classList.add("active"); // Display favorite colors
   }
 }
