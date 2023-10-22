a6c86834f08ac2aea5fcdd987432486d8f691c64
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-21 20:58:49 -0400

    Update min.ts

--- favorite_color_picker/src/main.ts
+++ favorite_color_picker/src/main.ts
@@ -1,13 +1,16 @@
 import saveColor from "./saveColor";
+import nextColor from "./nextColor";
 import getBgColor from "./getBgColor";
+import displayCurrentBg from "./displayCurrentBg";
 
 const saveColorBtn = document.querySelector(
   ".btn-wrapper__save-btn"
 ) as HTMLButtonElement;
-const nextColorBtn = document.querySelector(
+export const nextColorBtn = document.querySelector(
   ".btn-wrapper__next-btn"
 ) as HTMLButtonElement;
 
 window.addEventListener("DOMContentLoaded", getBgColor);
+window.addEventListener("DOMContentLoaded", displayCurrentBg);
 saveColorBtn.addEventListener("click", saveColor);
-nextColorBtn.addEventListener("click", getBgColor);
+nextColorBtn.addEventListener("click", nextColor);
