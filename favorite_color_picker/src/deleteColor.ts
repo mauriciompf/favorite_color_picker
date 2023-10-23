47017627f84d46ddf8b9760960195b1d3c5eee24
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-23 19:32:38 -0400

    Update deleteColor.ts

--- favorite_color_picker/src/deleteColor.ts
+++ favorite_color_picker/src/deleteColor.ts
@@ -1,3 +1,30 @@
-console.log();
+import { colorList } from "./colorList";
+import emptyColors from "./emptyColors";
 
-export default function deleteColor() {}
+export function deleteColor(itemList: HTMLElement, currentBodyBg: string) {
+  const index = colorList.indexOf(currentBodyBg);
+
+  // Check if the current bgColor exists in the colorList before attempting to remove it
+  if (index !== -1) {
+    colorList.splice(index, 1);
+  }
+
+  itemList.remove();
+}
+
+export function deleteAllColors(
+  deleteAllColorsButton: HTMLButtonElement,
+  colorListElem: HTMLElement
+) {
+  const listofColorsHTML = [...colorListElem.children];
+
+  listofColorsHTML.forEach((element) => {
+    element.remove();
+  });
+
+  colorList.splice(0, colorList.length); // Remove all elements from colorList array
+
+  deleteAllColorsButton.remove();
+
+  emptyColors();
+}
