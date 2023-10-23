f3d4738d36a5914452afb96bebe512a631a65a5e
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-22 20:40:23 -0400

    Update displayCurrentBg.ts

--- favorite_color_picker/src/displayCurrentBg.ts
+++ favorite_color_picker/src/displayCurrentBg.ts
@@ -7,3 +7,11 @@ export default function displayCurrentBg() {
     .getComputedStyle(document.body)
     .getPropertyValue("background-color");
 }
+
+//  keeps an eye on changes to the page's styling.
+const observer = new MutationObserver(() => {
+  displayCurrentBg();
+});
+
+const observerConfig = { attributes: true, attributeFilter: ["style"] };
+observer.observe(document.body, observerConfig);
