fe90c40eb147760007ab2b2dd753d6bcdf7e74ae
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-22 20:40:39 -0400

    Update nextColor.ts

--- favorite_color_picker/src/nextColor.ts
+++ favorite_color_picker/src/nextColor.ts
@@ -1,10 +1,5 @@
 import getBgColor from "./getBgColor";
 
-const arr: string[] = [];
 export default function nextColor() {
-  arr.push(
-    window.getComputedStyle(document.body).getPropertyValue("background-color")
-  );
-  console.log(arr);
   return getBgColor();
 }
