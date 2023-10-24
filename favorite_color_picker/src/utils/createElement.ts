83e01c63f91736d3754020daa71820f3c92123ff
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-24 19:50:16 -0400

    Update createElement.ts

--- favorite_color_picker/src/utils/createElement.ts
+++ favorite_color_picker/src/utils/createElement.ts
@@ -1,15 +1,24 @@
 export default function createElement<K extends keyof HTMLElementTagNameMap>(
   tagName: K,
-  attributes?: Record<string, string>
+  parent?: HTMLElement,
+  attributes?: Record<string, string>,
+  textContent?: string
 ): HTMLElementTagNameMap[K] {
   const element = document.createElement(tagName);
 
+  if (parent) {
+    parent.appendChild(element);
+  }
+
   if (attributes) {
     for (const key in attributes) {
-      if (attributes.hasOwnProperty(key)) {
+      if (attributes.hasOwnProperty(key))
         element.setAttribute(key, attributes[key]);
-      }
     }
+  }
+
+  if (textContent) {
+    element.textContent = textContent;
   }
 
   return element;
